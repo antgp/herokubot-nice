ecb8731219d8076a9286838a395d4424ecf23557
Antaney Gnanaraj Puvirajasingam <64874958+antgp@users.noreply.github.com>
2020-05-05 20:06:30 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,4 +17,4 @@ client.on('message', msg => {
   }
 });
 //IT HAS TO BE LIKE THIS
-client.login('NzA3MzczNDQzNTI3NDc1MjQw.XrH-dQ.z_7Lh44JOH8TspF4IabN9_VBkoM');
+client.login('NzA3MzczNDQzNTI3NDc1MjQw.XrH_MA.A6oqBbCFhQYiR-KwJGncgL4-eCg');
